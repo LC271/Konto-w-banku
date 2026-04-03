dd354eebd5e067e6d0542e5155ffb229d05d5953
LC271 <lukasz.ciochon@microsoft.wsei.edu.pl>
2026-04-03 21:21:26 +0200

    1.9

--- Bank/Bank/Konto.cs
+++ Bank/Bank/Konto.cs
@@ -22,18 +22,31 @@ namespace Bank
         public bool Zablokowane => zablokowane;
 
         public void Wplata(decimal kwota)
-        {     if (zablokowane)
+        {     
+            if (zablokowane)
             {
-                Console.WriteLine("Konto jest zablokowane. Nie można dokonać wpłaty.");
-                return;
+                throw new Exception("Konto jest zablokowane. Nie można dokonać wpłaty.");
             }
+
             if (kwota <= 0)
             {
-                Console.WriteLine("Kwota wpłaty musi być większa od zera.");
-                return;
+                throw new Exception("Kwota wpłaty musi być większa od zera.");
             }
+
             bilans += kwota;
-            Console.WriteLine($"Wpłacono {kwota:C}. Nowy bilans: {bilans:C}");
+        }
+        public void Wyplata(decimal kwota)
+        {
+            if (zablokowane)
+                throw new Exception("Konto jest zablokowane. Nie można dokonać wypłaty.");
+
+            if (kwota <= 0)
+                throw new Exception("Kwota wpłaty musi być większa od zera.");
+
+            if (kwota > bilans)
+                throw new Exception("Niewystarczające środki na koncie.");
+
+            bilans -= kwota;
         }
     }
 }
