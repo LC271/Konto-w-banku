878c51a943d5db4669cc519065a630d7809757db
LC271 <lukasz.ciochon@microsoft.wsei.edu.pl>
2026-04-03 21:02:17 +0200

    1.7

---
+++ Bank/Bank/Konto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank
+{
+    internal class Konto
+    {
+        private string klient;  //nazwa klienta
+        private decimal bilans;  //aktualny stan środków na koncie
+        private bool zablokowane = false; //stan konta
+
+        //private Konto() { } //konstruktor bezparametrowy, który jest prywatny, aby uniemożliwić tworzenie kont bez podania klienta i bilansu
+
+        public Konto(string klient, decimal bilansNaStart = 0) 
+        {
+            this.klient = klient;
+            this.bilans = bilansNaStart;
+        }
+
+        public string NazwaKlienta
+        {
+            get { return klient; }
+        }
+        public decimal Bilans
+        {
+            get { return bilans; }
+        }
+        public bool Zablokowane
+        {
+            get { return zablokowane; }
+        }
+    }
+}
