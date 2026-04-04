0bdf31980abd66266450a6192c25e72e83ba4cd1
LC271 <lukasz.ciochon@microsoft.wsei.edu.pl>
2026-04-04 17:26:58 +0200

    szkielet operacji

--- Bank/BankApp/Program.cs
+++ Bank/BankApp/Program.cs
@@ -17,6 +17,8 @@ class Program
 
         Console.WriteLine("program - Konto w banku");
         string[] rodzajeKonta = { "normalny", "plus", "z limitem debetowym" };
+        string[] operacje = { "Wpłata", "Wypłata", "Sprawdzenie bilansu", "Blokowanie konta", "Odblokowanie konta", "Wyjście" };
+
         static int WczytanieRodzaju(string komunikat)
         {
             int rodzaj;
@@ -36,5 +38,27 @@ class Program
 
         int RodzajKonta = WczytanieRodzaju("Podaj jaki rodzaj konta chcesz (np. normalne - 1, plus - 2, z limitem debetowym - 3): ");
         Console.WriteLine($"Wybrany rodzaj konta: {rodzajeKonta[RodzajKonta-1]}");
+
+        //operacje
+        Console.Write("\n-----[Możliwe operacje]-----\n");
+
+
+        static int WczytanieOperacji(string komunikat)
+        {
+            int rodzaj;
+
+            while (true)
+            {
+                Console.Write(komunikat);
+                string wejscie = Console.ReadLine();
+                if (int.TryParse(wejscie, out rodzaj))
+                {
+                    return rodzaj;
+                }
+                Console.WriteLine("Nieprawidłowy numer. Proszę spróbować użyć 1, 2 lub 3 do wyboru odpowiedniego rodzaju konta.");
+            }
+        }
+        int RodzajOperacji = WczytanieOperacji("1. Wpłata\n2. Wypłata\n3. Sprawdzenie bilansu\n4. Blokowanie konta\n5. Odblokowanie konta\n6. Wyjście\n\n");
+        Console.WriteLine($"Wybrany operacji: {operacje[RodzajOperacji - 1]}");
     }
 }
