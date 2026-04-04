f817357903828db2e1dd10a8b22bf895f7ea99b0
LC271 <lukasz.ciochon@microsoft.wsei.edu.pl>
2026-04-04 04:54:58 +0200

    szkielet kroku 4.

--- Bank/BankApp/Program.cs
+++ Bank/BankApp/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using Bank;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var konto = new Konto("Adam", 100);
+        Console.WriteLine($"Klient: {konto.Nazwa}, Bilans: {konto.Bilans}");
+
+        var konto2 = new Konto("Jan", 100);
+
+        konto2.Wplata(50);
+        konto2.Wyplata(30);
+
+        Console.WriteLine($"Klient: {konto2.Nazwa}, Bilans: {konto2.Bilans}");
+    }
+}
